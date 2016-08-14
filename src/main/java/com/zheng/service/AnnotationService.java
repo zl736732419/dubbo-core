ccdf101004c5ec9cb9c9c99e56761fcc1df2c9d0
zhenglian <zhenglian@192.168.56.1>
2016-08-15 00:20:03 +0800

    修改pom

---
+++ src/main/java/com/zheng/service/AnnotationService.java
@@ -0,0 +1,13 @@
+package com.zheng.service;
+
+/**
+ * 为了明显标注注解服务，
+ * 测试注解，命名就没有设定业务逻辑场景
+ * 
+ * @author zhenglian
+ *
+ */
+public interface AnnotationService {
+	
+	public String say(String str);
+}
